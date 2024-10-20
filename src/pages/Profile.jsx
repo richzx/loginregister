290eb514e3a71d2660ab7cee80d5861cd9ba69c1
richz <richarmiguelrodriguez@gmail.com>
2024-10-19 21:54:54 -0500

    update Profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,6 +10,7 @@ function Profile() {
 
   // hook details for user
   const [userDatil, setUserDetail] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   // constant fetch data for user
   const fetchUserData = async () => {
@@ -35,8 +36,11 @@ function Profile() {
   // function on logout
   async function handleLogout() {
     try {
-      await auth.signOut();
-      navigation('/');
+      setLoading(true);
+      setTimeout(function () {
+        auth.signOut();
+        navigation('/');
+      }, 500);
     } catch (error) {
       toast.error('Error al cerrar sesión: ', error.message)
     }
@@ -45,35 +49,44 @@ function Profile() {
   return (
     <main className='bg-[url("/img-bg.svg")] w-full h-screen flex justify-center items-center'>
       <section className='w-fit px-8 sm:px-16 py-6 sm:py-12 bg-white rounded-2xl flex flex-col items-center gap-5 sm:gap-10'>
-        <h2 className='text-3xl font-bold'>Tu perfil</h2>
-        {userDatil ? (
-          <div className='flex flex-col items-center gap-3'>
-            {userDatil.photo ? (
-              <img src={userDatil.photo} alt="imagen de perfil" className='w-[110px] object-cover rounded-full' />
-            ) : null}
-            <div className='flex flex-col gap-1'>
+        {loading ? (
+          <>
+            <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
+            {/* <h2 className='text-3xl font-bold'>Tu perfil</h2> */}
+          </>
+        ) : (
+          <>
+            <h2 className='text-3xl font-bold'>Tu perfil</h2>
+            {userDatil ? (
+              <div className='flex flex-col items-center gap-3'>
+                {userDatil.photo ? (
+                  <img src={userDatil.photo} alt="imagen de perfil" className='w-[110px] object-cover rounded-full' />
+                ) : null}
+                <div className='flex flex-col gap-1'>
 
-              <h2 className='text-2xl font-bold'>{userDatil.firstName}</h2>
-              <h2 className='flex gap-2 text-xl'>
-                Correo:
-                <span>{userDatil.email}</span>
-              </h2>
-              <h2 className='flex gap-2 text-xl'>
-                Nombre:
-                <span>{userDatil.firstName}</span>
-              </h2>
-              {userDatil.lastName ? (
-                <h2 className='flex gap-2 text-xl'>
-                  Apellido:
-                  <span>{userDatil.lastName}</span>
-                </h2>
-              ) : null}
-            </div>
-            <button onClick={handleLogout} className="w-full bg-gray-500 hover:bg-gray-700 text-white font-normal py-2 px-4 rounded-full">
-              Cerrar sesión
-            </button>
-          </div>
-        ) : 'cargando...'}
+                  <h2 className='text-2xl font-bold'>{userDatil.firstName}</h2>
+                  <h2 className='flex gap-2 text-xl'>
+                    Correo:
+                    <span>{userDatil.email}</span>
+                  </h2>
+                  <h2 className='flex gap-2 text-xl'>
+                    Nombre:
+                    <span>{userDatil.firstName}</span>
+                  </h2>
+                  {userDatil.lastName ? (
+                    <h2 className='flex gap-2 text-xl'>
+                      Apellido:
+                      <span>{userDatil.lastName}</span>
+                    </h2>
+                  ) : null}
+                </div>
+                <button onClick={handleLogout} className="w-full bg-gray-500 hover:bg-gray-700 text-white font-normal py-2 px-4 rounded-full">
+                  Cerrar sesión
+                </button>
+              </div>
+            ) : 'cargando...'}
+          </>
+        )}
       </section>
     </main>
   )
